c617e50cd173023c0f7f5c20ff388f1b26af58ae
subatomic450 <165378970+subatomic450@users.noreply.github.com>
2024-12-31 14:59:38 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    dbHost: 'my-3t-database.c9uyiyoko4n7.ap-south-1.rds.amazonaws.com',
+    dbUser: 'admin',
+    dbPassword: 'Iam123456',
+    dbName: 'user_management'
+};
+
+
+
+
